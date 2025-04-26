f77858611e12d4ebd769bbbae488cd0850f56cb7
MaksimAQr <aleksandrov3.mi@edu.spbstu.ru>
2025-04-26 14:58:10 +0300

    Alexandrov .cpp and .h files

---
+++ src/Pr1de/func_eval/func_eval.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include "func_eval.h"
+
+typedef void (*MyCallback)(EvalStatus status,
+            double value1, double value2, double value3, double value4,
+            Color local_color, Color external_color);
+
+const Color LOCAL_COLOR = {
+    .text = 13,
+    .background = 10,
+};
+
+namespace math_ut {
+    double next_el(size_t i, double x) {
+        return -1 * x*(i-1)/i;
+    }
+    double alexandrov_log(double eps, double x=0, double a=0, double next_el=0, size_t i=2) {
+        double cur_el;
+        if (i > 1000) {
+            return NAN;
+        }
+        if (i == 2) {
+            cur_el = x;
+        }
+        else {
+            cur_el = next_el;
+        }
+        double next = cur_el * math_ut::next_el(i, x);
+
+        if (abs(next - cur_el) < eps) {
+            return next_el;
+        }
+        return cur_el + alexandrov_log(eps, x, a, next, ++i);
+    }
+}
+
+using namespace math_ut;
+
+double myf_math(double x, double a) {
+    return log(x + 1);
+}
+
+const char* get_function_name()
+{
+    auto fn = "log(x + 1)";
+    return fn;
+}
+double generate_and_print_table_row(double x, double eps, double a, RowType row_type, Color external_color, MyCallback callback){
+    double my_func = math_ut::alexandrov_log(eps, x, a);
+    double math_func = myf_math(x, a);
+    double delta = sqrt(abs(pow(math_func, 2) - pow(my_func, 2)));
+
+    if (row_type == RowType::TABLE_1_ROW) {
+        callback(std::isnan(math_func) || std::isinf(abs(math_func)) ? EvalStatus::EVAL_ERROR : EvalStatus::EVAL_SUCCESS, x, my_func, math_func, delta, external_color, LOCAL_COLOR);
+    }
+    else{
+        callback(std::isnan(math_func) || std::isinf(abs(math_func)) ? EvalStatus::EVAL_ERROR : EvalStatus::EVAL_SUCCESS, eps, my_func, math_func, delta, external_color, LOCAL_COLOR);
+    }
+    return my_func;
+}
+
+// std::string Handle::setBackgroundColor(int color) {
+//     return "\033[38;5;" + std::to_string(color) + "m";
+// }
+// std::string Handle::setForegroundColor(int color) {
+//     return "\033[48;5;" + std::to_string(color) + "m";
+// }
+// std::string Handle::resetColor() {
+//     return "\033[0m";
+// }
---
+++ src/Pr1de/func_eval/func_eval.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#ifndef FUNC_EVAL_LIBRARY_H
+#define MYDLL_API __attribute__((visibility("default")))
+#define FUNC_EVAL_LIBRARY_H
+
+#endif //FUNC_EVAL_LIBRARY_H
+
+typedef struct {
+    int text;
+    int background;
+} Color;
+
+typedef enum {
+    EVAL_SUCCESS,
+    EVAL_ERROR
+} EvalStatus;
+
+typedef enum {
+    TABLE_1_ROW,
+    TABLE_2_ROW
+} RowType;
+
+typedef void (*MyCallback)(EvalStatus status,
+            double value1, double value2, double value3, double value4,
+            Color local_color, Color external_color);
+
+
+extern "C" MYDLL_API const char* get_function_name();
+
+extern "C" MYDLL_API double myf_math(double x, double a);
+
+extern "C" MYDLL_API double generate_and_print_table_row(
+    double x,
+    double eps,
+    double a,
+    RowType row_type,
+    Color external_color,
+    MyCallback callback);
